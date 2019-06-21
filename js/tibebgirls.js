315454bbcd3adfe6b13b74f5aa4a9e43820dcabb
Antonio Aspite <antonioaspite@gmail.com>
2019-06-21 15:46:43 +0300

    finishing responsive design and behind scenes carousel

--- js/tibebgirls.js
+++ js/tibebgirls.js
@@ -1,31 +1,28 @@
 jQuery(document).ready(function() {
-  /*
-          Carousel
-      */
-  $("#carousel-example").on("slide.bs.carousel", function(e) {
-    /*
-              CC 2.0 License Iatek LLC 2018
-              Attribution required
-          */
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 5;
-    var totalItems = $(".carousel-item").length;
-
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i = 0; i < it; i++) {
-        // append slides to end
-        if (e.direction == "left") {
-          $(".carousel-item")
-            .eq(i)
-            .appendTo(".carousel-inner");
-        } else {
-          $(".carousel-item")
-            .eq(0)
-            .appendTo(".carousel-inner");
-        }
-      }
-    }
+	$("#carousel-example").on("slide.bs.carousel", function(e) {
+	  var $e = $(e.relatedTarget);
+	  var idx = $e.index();
+	  console.log('idx' + idx)
+	  var itemsPerSlide = 5;
+	  var totalItems = $(".carousel-item").length;
+	  console.log('totalitems' + totalItems)
+  
+	  console.log('sdfsfsdf');
+  
+	  if (idx >= totalItems - (itemsPerSlide - 1)) {
+		var it = itemsPerSlide - (totalItems - idx);
+		for (var i = 0; i < it; i++) {
+		  // append slides to end
+		  if (e.direction == "left") {
+			$(".carousel-item")
+			  .eq(i)
+			  .appendTo(".carousel-inner");
+		  } else {
+			$(".carousel-item")
+			  .eq(0)
+			  .appendTo(".carousel-inner");
+		  }
+		}
+	  }
+	});
   });
-});
